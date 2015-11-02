0ae55b68c23ee82561af0f8de5e479491c4fd701
Dave Shawley <daves@aweber.com>
2015-11-02 14:06:09 -0500

    tests: Add recursively explosive test.

--- tests.py
+++ tests.py
@@ -1,8 +1,12 @@
+from __future__ import print_function
+
 import json
+import unittest
 
 from tornado import testing
 import msgpack
 
+from sprockets.mixins import mediatype
 import examples
 
 
@@ -66,3 +70,32 @@ class GetRequestBodyTests(testing.AsyncHTTPTestCase):
                               headers={'Content-Type': 'application/msgpack'})
         self.assertEqual(response.code, 200)
         self.assertEqual(json.loads(response.body.decode('utf-8')), body)
+
+
+class RegressionTests(unittest.TestCase):
+
+    def test_that_deeply_nested_json_is_supported(self):
+        # this generates as deeply nested of a JSON document as the
+        # underlying json.dumps can handle
+        depth = 0
+        nested = {'nested': 'dict'}
+        while True:
+            try:
+                parent = {'nested': nested}
+
+                def f():
+                    def g():
+                        json.dumps(parent)
+                    g()
+                f()
+                nested = parent
+                depth += 1
+            except RuntimeError:
+                break
+        print('this implementation can handle', depth, 'nesting levels')
+
+        handler = mediatype._TextContentHandler('application/json',
+                                                json.dumps, json.loads,
+                                                'utf-8')
+        _, encoded = handler.to_bytes(nested)
+        self.assertEqual(handler.from_bytes(encoded), nested)
