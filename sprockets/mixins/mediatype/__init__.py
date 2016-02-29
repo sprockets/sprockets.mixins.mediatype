d571209e38ee4f7ac7fee6c0a36be6b6b1bcf6c0
Dave Shawley <daves@aweber.com>
2016-02-29 14:35:07 -0500

    Remove _mark_deprecated wrapper.

--- sprockets/mixins/mediatype/__init__.py
+++ sprockets/mixins/mediatype/__init__.py
@@ -21,25 +21,8 @@ except ImportError as error:  # pragma no cover
     set_default_content_type = _error_closure
 
 
-def _mark_deprecated(func):
-    msg = '{0}.{1} is deprecated, use {0}.content.{1} instead'.format(
-        'sprockets.mixins.mediatype', func.__name__)
-
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        warnings.warn(msg, category=DeprecationWarning)
-        return func(*args, **kwargs)
-
-    return wrapper
-
-add_binary_content_type = _mark_deprecated(add_binary_content_type)
-add_text_content_type = _mark_deprecated(add_text_content_type)
-set_default_content_type = _mark_deprecated(set_default_content_type)
-ContentMixin = _mark_deprecated(ContentMixin)
-ContentSettings = _mark_deprecated(ContentSettings)
-
-version_info = (2, 0, 0)
+version_info = (2, 0, 1)
 __version__ = '.'.join(str(v) for v in version_info)
-__all__ = ('ContentMixin', 'ContentSettings', 'add_binary_content_type',
+__all__ = ['ContentMixin', 'ContentSettings', 'add_binary_content_type',
            'add_text_content_type', 'set_default_content_type',
-           'version_info', '__version__')
+           'version_info', '__version__']
