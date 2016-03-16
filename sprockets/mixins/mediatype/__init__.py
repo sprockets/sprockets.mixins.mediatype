3e9ad72d63adc7bf80bce44a4dd2158322518697
Dave Shawley <daves@aweber.com>
2016-03-16 09:19:43 -0400

    Metadata bumps for 2.1.0

--- sprockets/mixins/mediatype/__init__.py
+++ sprockets/mixins/mediatype/__init__.py
@@ -25,7 +25,7 @@ except ImportError as error:  # pragma no cover
     set_default_content_type = _error_closure
 
 
-version_info = (2, 0, 1)
+version_info = (2, 1, 0)
 __version__ = '.'.join(str(v) for v in version_info)
 __all__ = ['ContentMixin', 'ContentSettings', 'add_binary_content_type',
            'add_text_content_type', 'set_default_content_type',
