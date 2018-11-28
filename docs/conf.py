3aca38d60abb6f5359011b97ac58098042ab4c5a
Andrew Rabert <andrewr@aweber.com>
2018-11-28 16:26:35 -0500

    Update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinxcontrib.autohttp.tornado']
 master_doc = 'index'
 project = 'sprockets.mixins.mediatype'
-copyright = '2015-2016, AWeber Communications'
+copyright = '2015-2018, AWeber Communications'
 release = pkg_resources.get_distribution('sprockets.mixins.mediatype').version
 version = '.'.join(release.split('.')[0:1])
 
