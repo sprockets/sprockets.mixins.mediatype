e9e5f25925c800c76abe43efa62250bcc8fd7c79
Dave Shawley <daves@aweber.com>
2021-09-13 07:42:26 -0400

    Move metadata from setup.py into setup.cfg.

--- sprockets/mixins/mediatype/__init__.py
+++ sprockets/mixins/mediatype/__init__.py
@@ -1,29 +1,15 @@
-"""
-sprockets.mixins.mediatype
-
-"""
+"""sprockets.mixins.mediatype"""
 try:
     from .content import (ContentMixin, ContentSettings,
                           add_binary_content_type, add_text_content_type,
                           set_default_content_type)
 except ImportError as error:  # noqa: F841  # pragma no cover
-    def _error_closure(*args, **kwargs):
-        raise error  # noqa: F821
-
-    class ErrorClosureClass(object):
-        def __init__(self, *args, **kwargs):
-            raise error  # noqa: F821
-
-    ContentMixin = ErrorClosureClass
-    ContentSettings = ErrorClosureClass
-    add_binary_content_type = _error_closure
-    add_text_content_type = _error_closure
-    set_default_content_type = _error_closure
+    import warnings
+    warnings.warn(
+        'Missing runtime requirements for sprockets.mixins.mediatype',
+        UserWarning)
 
 
 version_info = (3, 0, 4)
-__version__ = '.'.join(str(x) for x in version_info)
-
-__all__ = ['ContentMixin', 'ContentSettings', 'add_binary_content_type',
-           'add_text_content_type', 'set_default_content_type',
-           'version_info', '__version__']
+version = '.'.join(str(x) for x in version_info)
+__version__ = version  # compatibility
