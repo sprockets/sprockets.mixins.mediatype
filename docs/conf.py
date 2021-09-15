e26a94778b99fbe55ee84b9aab64f23670a1b5c9
Dave Shawley <daves@aweber.com>
2021-09-15 06:58:21 -0400

    Using sphinx-rtd-theme locally as well.

--- docs/conf.py
+++ docs/conf.py
@@ -1,37 +1,35 @@
+import os
+
 import pkg_resources
 
-needs_sphinx = '1.0'
+needs_sphinx = '4.0'
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
               'sphinx.ext.intersphinx',
               'sphinx.ext.extlinks',
-              'sphinxcontrib.autohttp.tornado']
+              'sphinxcontrib.httpdomain']
 master_doc = 'index'
 project = 'sprockets.mixins.mediatype'
 copyright = '2015-2021, AWeber Communications'
 release = pkg_resources.get_distribution('sprockets.mixins.mediatype').version
 version = '.'.join(release.split('.')[0:1])
 
-html_style = 'custom.css'
-html_static_path = ['static']
+# Only install the ReadTheDocs theme when we are not running
+# in the RTD build system.
+if not os.environ.get('READTHEDOCS', None):
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 html_sidebars = {
     '**': ['about.html', 'navigation.html'],
-}
-html_theme_options = {
-    'github_user': 'sprockets',
-    'github_repo': 'sprockets.mixins.mediatype',
-    'description': 'Content-Type negotation mix-in',
-    'github_banner': True,
-    'travis_button': True,
-    'sidebar_width': '230px',
-    'codecov_button': True,
 }
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
     'requests': ('https://requests.readthedocs.org/en/latest/', None),
     'sprockets': ('https://sprockets.readthedocs.org/en/latest/', None),
-    'tornado': ('http://tornadoweb.org/en/latest/', None),
+    'tornado': ('https://www.tornadoweb.org/en/stable/', None),
 }
 
 # https://www.sphinx-doc.org/en/master/usage/extensions/extlinks.html
